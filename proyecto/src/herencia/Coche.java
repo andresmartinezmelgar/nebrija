a7ca012d159830c9597fd9c53887eb410e0ce62a
andres <andresmartinezmelgar@gmail.com>
2026-02-11 12:03:27 +0100

    cambio desde la rama de trabajo

--- proyecto/src/herencia/Coche.java
+++ proyecto/src/herencia/Coche.java
@@ -7,10 +7,8 @@ public class Coche {
 	protected int numVentanas;
 	protected int numAsientos;
 	protected int velocidadMax;
-	
-	public Coche(int numRuedas, int numPuertas,
-			String modelo, int numVentanas,
-			int numAsientos, int velocidadMax) {
+
+	public Coche(int numRuedas, int numPuertas, String modelo, int numVentanas, int numAsientos, int velocidadMax) {
 		this.numRuedas = numRuedas;
 		this.numPuertas = numPuertas;
 		this.modelo = modelo;
@@ -18,27 +16,14 @@ public class Coche {
 		this.numAsientos = numAsientos;
 		this.velocidadMax = velocidadMax;
 	}
-	
-	
+
+	public void arrancar() {
+		System.out.println("Brum brum!");
+	}
+
 	public void mostrarInfo() {
-		System.out.println("Info general del coche\n"
-				+ "\tNumRuedas:" +this.numRuedas+"\n"
-				+ "\tnumPuertas:" +this.numPuertas+"\n"
-				+ "\tmodelo:" +this.modelo+"\n"
-				+ "\tnumVentanas:" +this.numVentanas+"\n"
-				+ "\tnumAsientos:" +this.numAsientos+"\n"
-				+ "\tvelocidadMax:" +this.velocidadMax+""
-				);
+		System.out.println("Info general del coche\n" + "\tNumRuedas:" + this.numRuedas + "\n" + "\tnumPuertas:"
+				+ this.numPuertas + "\n" + "\tmodelo:" + this.modelo + "\n" + "\tnumVentanas:" + this.numVentanas + "\n"
+				+ "\tnumAsientos:" + this.numAsientos + "\n" + "\tvelocidadMax:" + this.velocidadMax + "");
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
